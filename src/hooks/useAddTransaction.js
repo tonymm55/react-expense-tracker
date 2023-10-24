b29e72fd0fb56ce9704d2cc80b3d71eddabec2c4
TonyMM55 <tonymmoran@gmail.com>
2023-10-24 17:29:38 +0100

    added react hooks and events

--- src/hooks/useAddTransaction.js
+++ src/hooks/useAddTransaction.js
@@ -1,5 +1,6 @@
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db } from "../config/firebase-config";
+import { useGetUserInfo } from "./useGetUserInfo";
 
 export const useAddTransaction = () => {
   const transactionCollectionRef = collection(db, "transactions"); 
--- src/pages/expense-tracker/index.jsx
+++ src/pages/expense-tracker/index.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 import { useAddTransaction } from '../../hooks/useAddTransaction.js'
 
 export const ExpenseTracker = () => {
   const { addTransaction } = useAddTransaction();
 
-  const onSubmit = (event) => {
-    event.preventDefault()
-    addTransaction({description: "Haircut", transactionAmount: 10, transactionType: "expense"});
+  const [description, setDescription] = useState("");
+  const [transactionAmount, setTransactionAmount] = useState(0);
+  const [transactionType, setTransactionType] = useState("expense");
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addTransaction({
+        description, 
+        transactionAmount, 
+        transactionType,
+    });
   };
 
   return (
@@ -26,12 +36,35 @@ export const ExpenseTracker = () => {
                 <h2>£0.00</h2>
             </div>
             <form className="add-transaction" onSubmit={onSubmit}>
-                <input type="text" placeholder="Description" required />
-                <input type="number" placeholder="Amount" required />
-                <input type="radio" id="expense" value="expense" />
-                <label htmlFor="expense">Expense</label>
-                <input type="radio" id="income" value="income" />
-                <label htmlFor="income">Income</label>
+                <input 
+                  type="text" 
+                  placeholder="Description" 
+                  required 
+                  onChange={(e) => setDescription(e.target.value)} 
+                />
+
+                <input 
+                  type="number" 
+                  placeholder="Amount" 
+                  required 
+                  onChange={(e) => setTransactionAmount(e.target.value)}
+                />
+                <input 
+                  type="radio" 
+                  id="expense" 
+                  value="expense" 
+                  checked={transactionType === "expense"}
+                  onChange={(e) => setTransactionType(e.target.value)}
+                />
+                <label htmlFor="expense"> Expense</label>
+                <input 
+                  type="radio" 
+                  id="income" 
+                  value="income" 
+                  checked={transactionType === "income"}
+                  onChange={(e) => setTransactionType(e.target.value)}
+                />
+                <label htmlFor="income"> Income</label>
 
                 <button type="submit">Add Transaction</button>
             </form>
